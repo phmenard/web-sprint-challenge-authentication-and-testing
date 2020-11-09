119323db74dfce221672d065554eecb6475db91a
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-11-08 20:40:31 -0500

    fixed up routers for user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,17 +1,70 @@
 const router = require('express').Router();
-const users = require("./users-model")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const Users = require("./users-model")
 
 router.post('/register',  async (req, res, next) => {
-	try {	
-		const user = await users.create(req.body)
-		res.status(201).json(user)
-	} catch (error) {
+  // impliment register
+  try {
+		const { username, password } = req.body
+		const user = await Users.findByUsername(username)
+
+		if (user) {
+			return res.status(409).json({
+				message: "Username is already taken",
+			})
+		}
+
+		const newUser = await Users.create({
+			username,
+			// hash the password with a time complexity of "14"
+			password: await bcrypt.hash(password, 14),
+		})
+
+		res.status(201).json(newUser)
+	} catch(error) {
 		next(error)
 	}
 })
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res, next) => {
   // implement login
+  try {
+    const {username, password} = req.body
+    const user = await Users.findByUsername(username)
+
+    if(!user) {
+      return res.status(401).json({
+        message: "Invalid user name"
+      })
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password)
+
+    if (!passwordValid){
+      return res.status(401).json({
+        message: "Invalid password"
+      })
+    }
+
+    const token = jwt.sign({
+      id: user.id,
+      username: user.username,
+
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.json({
+      message: `Welcome ${user.username}`,
+    })
+
+
+
+
+  } catch (error) {
+    next(error)
+  }
 });
 
 module.exports = router;
