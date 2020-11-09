4ee357103613e77fbf84fb5d4ac28dd7dfe326e4
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-11-08 19:28:46 -0500

    added register router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const users = require("./users-model")
 
-router.post('/register', (req, res) => {
-  // implement registration
-});
+router.post('/register',  async (req, res, next) => {
+	try {	
+		const user = await users.create(req.body)
+		res.status(201).json(user)
+	} catch (error) {
+		next(error)
+	}
+})
 
 router.post('/login', (req, res) => {
   // implement login
